72361cb04e4c65119b1786bc6e3363f91e8f8440
Rob Howard <rob@robhoward.id.au>
2016-01-07 00:56:49 +1100

    cloud-to-butt => news-to-butt; uses the xkcd #1625 substitution list make the news more fun.

--- README.md
+++ README.md
@@ -1,38 +1,15 @@
-cloud-to-butt
-=============
+# news-to-butt
 
-![](logo.png)
+Chrome extension that replaces occurrences of words on the left with words on the right (from [xkcd #1625](https://xkcd.com/1625/)):
 
-Chrome extension that replaces occurrences of 'the cloud' with 'my butt'
+[![xkcd #1625](https://camo.githubusercontent.com/35da079d805943d8071b111412c79cbe07cae785/68747470733a2f2f696d67732e786b63642e636f6d2f636f6d6963732f737562737469747574696f6e735f322e706e67)](https://xkcd.com/1625/)
 
-[Direct download of crx file](https://github.com/panicsteve/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
+[Direct download of crx file](https://github.com/damncabbage/news-to-butt/blob/master/NewsToButt.crx?raw=true)
 
-Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
-In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
-polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_ extension replaces only the phrase described above. Thank you for your concern.
-
-Screenshot Gallery
-------------------
-
-http://www.flickr.com/groups/cloud-to-butt/
-
-Installation
-------------
-
-In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
-
-Safari Version
---------------
-
-Can be found here: https://github.com/logancollins/cloud-to-butt-safari
-
-Firefox Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-mozilla
+(Named after Steven Frank's [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) extension.)
 
 
-Opera Version
----------------
+## Installation
 
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-opera
+In Chrome, choose Window > Extensions. Drag NewsToButt.crx into the page that appears.
+
--- Source/content_script.js
+++ Source/content_script.js
@@ -1,10 +1,9 @@
-walk(document.body);
+"use strict";
 
 function walk(node) 
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
 	var child, next;
 
 	switch ( node.nodeType )  
@@ -22,21 +21,82 @@ function walk(node)
 			break;
 
 		case 3: // Text node
-			handleText(node);
+			node.nodeValue = handleText(node.nodeValue);
 			break;
 	}
 }
 
-function handleText(textNode) 
+function matchCaseOfLeft(left, right)
 {
-	var v = textNode.nodeValue;
+  var capitalise = function(x) {
+    var firstAlphaIdx = x.search(/[a-z]/i);;
+    if (firstAlphaIdx == -1 ) return x;
+    return x.substring(0, firstAlphaIdx) + x.charAt(firstAlphaIdx).toUpperCase() + x.substring(firstAlphaIdx + 1);
+  };
+  var isCapitalised = function(x) {
+    return x == capitalise(x);
+  }
+  var leftCapitalisedCount = left.reduce(function(count, part){
+    return isCapitalised(part) ? count + 1 : count;
+  }, 0);
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  // Capitalise as many words on the right as there are capitalised on the left.
+  // Special case: if left is all-caps, then capitalise all of the right.
+  var leftAllCaps = (leftCapitalisedCount == left.length)
+  return right.map(function(rightWord, idx) {
+    var shouldCaps = (left[idx] ? isCapitalised(left[idx]) : leftAllCaps);
+    return shouldCaps ? capitalise(rightWord) : rightWord;
+  });
 }
 
+function handleText(value) {
+  var mappings = [
+    [ "debate",             function()  { return ["dance","-","off"] }],
+    [ "self([- ])driving",  function(d) { return ["uncontrollably",d,"swerving"] }],
+    [ "poll(ing)?",               function()  { return ["psychic"," ","reading"] }],
+    [ "candidate",          function()  { return ["airbender"] }],
+    [ "drone",              function()  { return ["dog"] }],
+    [ "vows to",            function()  { return ["probably"," ","won't"] }],
+    [ "at large",           function()  { return ["very"," ","large"] }],
+    [ "successfully",       function()  { return ["suddenly"] }],
+    [ "expands",            function()  { return ["physically"," ","expands"] }],
+    [ "an unknown number",  function()  { return ["like"," ","hundreds"] }],
+    [ "front([- ])runner",  function(d) { return ["blade",d,"runner"] }],
+    [ "global",             function()  { return ["spherical"] }],
+    [ "years",              function()  { return ["minutes"] }], // Doing the inverse is way too annoying to pull off.
+    [ "no indication",      function()  { return ["lots"," ","of"," ","signs"] }],
+    [ "horsepower",         function()  { return ["tons"," ","of"," ","horsemeat"] }],
+    [ "urged restraint by", function()  { return ["drunkenly"," ","egged"," ","on"] }],
+    [ "(?:first|second|third)([- ])degree", function(d) { return ["friggin'"," ","awful"] }]
+  ];
+
+  return mappings.reduce(function(result, mapping) {
+    var left = mapping[0];
+    var right = mapping[1];
+
+    return result.replace(new RegExp(left, 'ig'), function(){ //match){
+      // Ignore first argument (match) and last two arguments (replacement location, full string)
+      var args = Array.prototype.slice.call(arguments, 0);
+      var match = args[0];
+      var refs = args.splice(1, (args.length - 3));
+
+      var substituted = right.apply(null, refs); // Run the replacement function with any potential replacements from the left.
+      return matchCaseOfLeft(match.split(/\b/), substituted).join(''); // Replace the left with the matched-caps right.
+    });
+  }, value);
+}
+
+if (typeof root == 'undefined') {
+  // Chrome plugin
+  walk(document.body);
+} else {
+  // We're inside Node.js
+  module.exports = { handleText: handleText };
+}
+
+// Testing (TODO: doctest)
+// Try:
+//   require('./Source/content_script').handleText("No Indication of Horsepower: Today's debate featuring self-Driving cars sent an unknown number of Drone candidates to successfully expand their global polling numbers. The first-degree front-runner still remains at large.");
+// Expect:
+//   "Lots Of Signs of Tons Of Horsemeat: Today's dance-off featuring uncontrollably-Swerving cars sent like hundreds of Dog airbenders to suddenly expand their spherical psychic reading numbers. The friggin' awful blade-runner still remains very large."
 
